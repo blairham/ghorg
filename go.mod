55f0c23be7ec757bd64f9633a0faa63168fd558a
Blair Hamilton <blairham@me.com>
2025-12-16 19:02:30 -0500

    refactor: replace fatih/color with charmbracelet/log in colorlog package
    - Use charmbracelet/lipgloss for color styling in PrintInfo, PrintSuccess,
      PrintError, and PrintSubtleInfo functions
    - Add structured logging functions (Info, Error, Debug, Warn, Fatal) using
      charmbracelet/log for future use
    - Add formatMsg helper to convert any type to string
    - Maintain backward compatibility with existing GHORG_COLOR and GHORG_QUIET
      environment variable behavior
    
    Note: fatih/color remains as an indirect dependency since it's used by
    briandowns/spinner and mitchellh/cli packages.

--- go.mod
+++ go.mod
@@ -6,8 +6,9 @@ require (
 	code.gitea.io/sdk/gitea v0.22.1
 	github.com/bradleyfalzon/ghinstallation/v2 v2.17.0
 	github.com/briandowns/spinner v1.23.2
+	github.com/charmbracelet/lipgloss v1.1.0
+	github.com/charmbracelet/log v0.4.2
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc
-	github.com/fatih/color v1.18.0
 	github.com/google/go-github/v72 v72.0.0
 	github.com/jessevdk/go-flags v1.6.1
 	github.com/knadh/koanf/parsers/yaml v1.1.0
@@ -29,10 +30,17 @@ require (
 	github.com/Masterminds/semver/v3 v3.4.0 // indirect
 	github.com/Masterminds/sprig/v3 v3.3.0 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/bgentry/speakeasy v0.2.0 // indirect
+	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/davidmz/go-pageant v1.0.2 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-fed/httpsig v1.1.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/google/go-github/v75 v75.0.0 // indirect
@@ -45,16 +53,22 @@ require (
 	github.com/hashicorp/go-version v1.8.0 // indirect
 	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/knadh/koanf/maps v0.1.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/posener/complete v1.2.3 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/exp v0.0.0-20250813145105-42675adae3e6 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/term v0.38.0 // indirect
--- internal/colorlog/colorlog.go
+++ internal/colorlog/colorlog.go
@@ -4,9 +4,46 @@ package colorlog
 import (
 	"fmt"
 	"os"
+	"sync"
 
-	"github.com/fatih/color"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
 )
+
+var (
+	logger     *log.Logger
+	loggerOnce sync.Once
+)
+
+// getLogger returns the configured logger instance (singleton)
+func getLogger() *log.Logger {
+	loggerOnce.Do(func() {
+		logger = log.NewWithOptions(os.Stdout, log.Options{
+			ReportTimestamp: false,
+			ReportCaller:    false,
+		})
+
+		// Configure based on GHORG_COLOR setting
+		if os.Getenv("GHORG_COLOR") != "enabled" {
+			// Disable colors by using a no-color profile
+			logger.SetStyles(log.DefaultStyles())
+			logger.SetColorProfile(0) // No color
+		}
+	})
+	return logger
+}
+
+// formatMsg converts any type to string
+func formatMsg(msg any) string {
+	switch v := msg.(type) {
+	case string:
+		return v
+	case error:
+		return v.Error()
+	default:
+		return fmt.Sprint(v)
+	}
+}
 
 // PrintInfo prints yellow colored text to standard out
 func PrintInfo(msg any) {
@@ -14,43 +51,37 @@ func PrintInfo(msg any) {
 		return
 	}
 
-	switch os.Getenv("GHORG_COLOR") {
-	case "enabled":
-		color.New(color.FgYellow).Println(msg)
-	default:
-		fmt.Println(msg)
+	if os.Getenv("GHORG_COLOR") == "enabled" {
+		style := lipgloss.NewStyle().Foreground(lipgloss.Color("11")) // Yellow
+		fmt.Println(style.Render(formatMsg(msg)))
+	} else {
+		fmt.Println(formatMsg(msg))
 	}
 }
 
 // PrintSuccess prints green colored text to standard out
 func PrintSuccess(msg any) {
-	switch os.Getenv("GHORG_COLOR") {
-	case "enabled":
-		color.New(color.FgGreen).Println(msg)
-	default:
-		fmt.Println(msg)
+	if os.Getenv("GHORG_COLOR") == "enabled" {
+		style := lipgloss.NewStyle().Foreground(lipgloss.Color("10")) // Green
+		fmt.Println(style.Render(formatMsg(msg)))
+	} else {
+		fmt.Println(formatMsg(msg))
 	}
 }
 
 // PrintError prints red colored text to standard out
 func PrintError(msg any) {
-	switch os.Getenv("GHORG_COLOR") {
-	case "enabled":
-		color.New(color.FgRed).Println(msg)
-	default:
-		fmt.Println(msg)
+	if os.Getenv("GHORG_COLOR") == "enabled" {
+		style := lipgloss.NewStyle().Foreground(lipgloss.Color("9")) // Red
+		fmt.Println(style.Render(formatMsg(msg)))
+	} else {
+		fmt.Println(formatMsg(msg))
 	}
 }
 
 // PrintErrorAndExit prints red colored text to standard out then exits 1
 func PrintErrorAndExit(msg any) {
-	switch os.Getenv("GHORG_COLOR") {
-	case "enabled":
-		color.New(color.FgRed).Println(msg)
-	default:
-		fmt.Println(msg)
-	}
-
+	PrintError(msg)
 	os.Exit(1)
 }
 
@@ -60,10 +91,47 @@ func PrintSubtleInfo(msg any) {
 		return
 	}
 
-	switch os.Getenv("GHORG_COLOR") {
-	case "enabled":
-		color.New(color.FgHiMagenta).Println(msg)
-	default:
-		fmt.Println(msg)
+	if os.Getenv("GHORG_COLOR") == "enabled" {
+		style := lipgloss.NewStyle().Foreground(lipgloss.Color("13")) // Magenta
+		fmt.Println(style.Render(formatMsg(msg)))
+	} else {
+		fmt.Println(formatMsg(msg))
 	}
+}
+
+// For future use - structured logging functions using charmbracelet/log
+
+// Info logs an info message with structured logging
+func Info(msg string, keyvals ...interface{}) {
+	if os.Getenv("GHORG_QUIET") == "true" {
+		return
+	}
+	getLogger().Info(msg, keyvals...)
+}
+
+// Error logs an error message with structured logging
+func Error(msg string, keyvals ...interface{}) {
+	getLogger().Error(msg, keyvals...)
+}
+
+// Debug logs a debug message with structured logging
+func Debug(msg string, keyvals ...interface{}) {
+	if os.Getenv("GHORG_DEBUG") == "true" {
+		getLogger().Debug(msg, keyvals...)
+	}
+}
+
+// Warn logs a warning message with structured logging
+func Warn(msg string, keyvals ...interface{}) {
+	getLogger().Warn(msg, keyvals...)
+}
+
+// Fatal logs a fatal message and exits
+func Fatal(msg string, keyvals ...interface{}) {
+	getLogger().Fatal(msg, keyvals...)
+}
+
+// SetTimeFunction allows setting a custom time function (useful for testing)
+func SetTimeFunction(f log.TimeFunction) {
+	getLogger().SetTimeFunction(f)
 }
