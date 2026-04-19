bb6479f97e1d7b6e08433c7e32d20c6ec98098d6
Blair Hamilton <blairham@me.com>
2026-04-18 22:49:05 -0400

    feat(cli): add global --color flag
    Parses a global --color flag before dispatching to
    subcommands, setting GHORG_COLOR to enable colored output.

--- cmd/ghorg/main.go
+++ cmd/ghorg/main.go
@@ -5,13 +5,28 @@ import (
 	"os"
 
 	"github.com/hashicorp/cli"
+	"github.com/jessevdk/go-flags"
 
 	"github.com/blairham/ghorg/internal/cmd"
 )
 
+// GlobalFlags holds flags that apply to all commands
+type GlobalFlags struct {
+	Color bool `long:"color" description:"Enable colorful output"`
+}
+
 func main() {
+	// Parse global flags first, passing remaining args to hashicorp/cli
+	var globalOpts GlobalFlags
+	parser := flags.NewParser(&globalOpts, flags.IgnoreUnknown)
+	remaining, _ := parser.ParseArgs(os.Args[1:])
+
+	if globalOpts.Color {
+		os.Setenv("GHORG_COLOR", "enabled")
+	}
+
 	c := cli.NewCLI("ghorg", cmd.GetVersion())
-	c.Args = os.Args[1:]
+	c.Args = remaining
 	c.Commands = cmd.CommandFactory()
 
 	exitStatus, err := c.Run()
